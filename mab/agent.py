fe815977237ec69641c21d387e38823663de14c3
Meinczinger, Tibor <tibor.meinczinger@merck.com>
2021-02-27 17:29:35 +0100

    Action selector based on upper confidence bound added

--- mab/agent.py
+++ mab/agent.py
@@ -1,4 +1,5 @@
 import numpy as np
+from enum import Enum
 
 
 class RewardStrategy:
@@ -57,9 +58,64 @@ class Environment:
         return self.arm[arm].reward_strategy.reward()
 
 
+class ActionSelector:
+    def __init__(self):
+        pass
+
+    def select_action(self, q_values: [float], arm_count: [int], step: int) -> int:
+        raise NotImplementedError
+
+
+class EpsilonGreedyActionSelector:
+    """
+    With probability 1-epsilon, choose the action with the highest value (or randomly among them if
+    there are more than one. Otherwise choose a random action to support exploration
+    """
+    def __init__(self, epsilon):
+        super(EpsilonGreedyActionSelector, self).__init__()
+        self.epsilon = epsilon
+
+    def select_action(self, q_values: [float], arm_count: [int], step: int) -> int:
+        if np.random.rand() < self.epsilon:
+            return np.random.choice(len(q_values))
+        else:
+            return np.random.choice(np.flatnonzero(q_values == q_values.max()))
+
+
+class UpperConfidenceBoundActionSelector:
+    """
+    Choose the action with the highest upper confidence bound value
+    """
+    def __init__(self, c):
+        super(UpperConfidenceBoundActionSelector, self).__init__()
+        self.c = c
+
+    def select_action(self, q_values: [float], arm_count: [int], step: int) -> int:
+        return np.argmax(q_values + self.c*np.sqrt(np.log(step)/(arm_count+1.0)))
+
+
+class ActionSelectorEnum(Enum):
+    EPSILON_GREEDY = 0
+    UPPER_CONFIDENCE_BOUND = 1
+
+
+ACTION_SELECTOR_TYPE = {ActionSelectorEnum.EPSILON_GREEDY: EpsilonGreedyActionSelector,
+                        ActionSelectorEnum.UPPER_CONFIDENCE_BOUND: UpperConfidenceBoundActionSelector}
+
+
+class ActionSelectorFactory(object):
+    @staticmethod
+    def get_action_selector(action_selector_type: ACTION_SELECTOR_TYPE, **kwargs):
+        try:
+            return ACTION_SELECTOR_TYPE[action_selector_type](**kwargs)
+        except Exception:
+            raise Exception
+
+
 class Agent:
     """ Class representing the agent """
-    def __init__(self, environment: Environment, epsilon: float, initial_value: float = 0.0):
+    def __init__(self, environment: Environment, epsilon: float,
+                 action_selector: ActionSelector, initial_value: float = 0):
         """
         environment - the environment the agent is going to interact with
         epsilon - the value of epsilon to control exploration
@@ -67,18 +123,10 @@ class Agent:
         """
         self.env = environment
         self.epsilon = epsilon
+        self.action_selector = action_selector
         self.q_values = np.full(self.env.get_nr_of_arms(), initial_value)
         self.arm_counts = np.zeros(self.env.get_nr_of_arms())
-
-    def choose_action(self) -> int:
-        """
-        Chooses the best action, in case of a tie, choose randomly.
-        Use epsilon to decide when to explore (choose a random action)
-        """
-        if np.random.rand() < self.epsilon:
-            return np.random.choice(len(self.q_values))
-        else:
-            return np.random.choice(np.flatnonzero(self.q_values == self.q_values.max()))
+        self.step = 1
 
     def agent_step(self) -> (int, float):
         """ Performs one step
@@ -87,19 +135,23 @@ class Agent:
             reward - the reward received
         """
         # Find the action with the highest value
-        action = self.choose_action()
+        action = self.action_selector.select_action(self.q_values, self.arm_counts, self.step)
         # Get the reward from the environment
         reward = self.env.reward(action)
         self.arm_counts[action] += 1
         # Update the estimated action value
         self.q_values[action] = self.q_values[action] + \
                                 (reward - self.q_values[action]) / self.arm_counts[action]
+        # Keep the number of steps the agent made up-to-date
+        self.step += 1
+
         return action, reward
 
 
 class Simulation:
     """ Class to run a simulation """
-    def __init__(self, nr_of_arms: int, epsilon: float, nr_of_steps: int, initial_value: float = 0.0):
+    def __init__(self, nr_of_arms: int, epsilon: float, nr_of_steps: int,
+                 action_selector: ActionSelector, initial_value: float = 0.0):
         """
         nr_of_arms - how many arms the environment will have
         epsilon - parameter to contorl exploration
@@ -110,6 +162,7 @@ class Simulation:
         self.epsilon = epsilon
         self.nr_of_steps = nr_of_steps
         self.initial_value = initial_value
+        self.action_selector = action_selector
 
     def simulate(self) -> [float]:
         """
@@ -124,7 +177,7 @@ class Simulation:
         # Create the environment
         env = Environment(arms)
         # Create the agent
-        agent = Agent(env, self.epsilon, self.initial_value)
+        agent = Agent(env, self.epsilon, self.action_selector, self.initial_value)
 
         scores = [0]
         averages = []
