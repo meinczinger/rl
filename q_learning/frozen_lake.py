24a8937a66009f9f06aafb60c23dc2adc8c63536
meinczinger <meinczinger@gmail.com>
2023-01-22 23:47:45 +0100

    q learning

--- q_learning/frozen_lake.py
+++ q_learning/frozen_lake.py
@@ -14,12 +14,11 @@ class Moves(IntEnum):
 
 class FrozenLake:
     def __init__(
-        self, epsilon: float = 0.3, epsilon_final: float = 0.01, epsilon_decay: float = 0.95, gamma: float = 0.9, table_size: int = 4
+        self, epsilon: float = 0.3, gamma: float = 1, alpha: float = 0.0, table_size: int = 4
     ) -> None:
         self._epsilon = epsilon
-        self._epsilon_final = epsilon_final
-        self._epsilon_decay = epsilon_decay
         self._gamma = gamma
+        self._alpha = alpha
         self._table_size = table_size
         self._env = self.create_env()
         self._q_values = torch.zeros(
@@ -28,7 +27,7 @@ class FrozenLake:
         self._visited_counts = Counter()
 
     def create_env(self):
-        return gym.make("FrozenLake-v1", is_slippery=False)
+        return gym.make("FrozenLake-v1", is_slippery=True)
 
     def learn(self):
         self._fvmc.learn(10)
@@ -51,46 +50,38 @@ class FrozenLake:
         steps_total = []
         rewards_total = []
 
-        epsilon = self._epsilon
         for _ in range(nr_of_episodes):
             step = 0
             state, _ = self._env.reset()
 
-
             while True:
                 step += 1
-
-                if epsilon < torch.rand(1)[0]:
-                    values_with_noise = (
-                        self._q_values[state]
-                        + torch.randn(1, self._env.action_space.n) / 1000.0
-                    )
-
-                    action = int(torch.max(values_with_noise, 1)[1])
-                else:
-                    action = self._env.action_space.sample()
-
-                if epsilon > self._epsilon_final:
-                    epsilon *= self._epsilon_decay  
+                values_with_noise = (
+                    self._q_values[state]
+                    + torch.randn(1, self._env.action_space.n) / 1000.0
+                )
+                action = int(torch.max(values_with_noise, 1)[1])
 
                 new_state, reward, terminated, aborted, info = self._env.step(action)
 
-                self._q_values[state, action] = reward + self._gamma * torch.max(
+                self._q_values[state, action] = (1-self._alpha) * self._q_values[state, action] + self._alpha * (reward + self._gamma * torch.max(
                     self._q_values[new_state]
-                )
+                ))
 
                 state = new_state
 
                 if terminated:
                     steps_total.append(step)
                     rewards_total.append(reward)
-                    print("Episode finished after {0} steps".format(step))
+                    # print("Episode finished after {0} steps".format(step))
                     break
 
         print(self._q_values)
         print("Average number of steps {}".format(sum(steps_total) / nr_of_episodes))
         print("Average number of steps for last 100 episodes {}".format(sum(steps_total[-100:]) / 100))
         print("Percentage of successful episodes {}".format(sum(rewards_total)/nr_of_episodes))
+        print("Percentage of successful episodes for the last 100 episodes {}".format(sum(rewards_total[-100:])/100.0))
 
-mc = FrozenLake(epsilon=0.7)
+
+mc = FrozenLake(0.3, 0.95, 0.9)
 mc.generate_episode(1000)
